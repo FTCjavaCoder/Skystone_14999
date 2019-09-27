4ef3e80a5becaea6778b76c777e16f8dc4f01d3b
Caleb <calebspiess@gmail.com>
2019-09-27 13:48:38 -0700

    Fixed drive general test

--- OpModes/Autonomous/DriveGeneralTest.java
+++ OpModes/Autonomous/DriveGeneralTest.java
@@ -2,6 +2,8 @@ package Skystone_14999.OpModes.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import Skystone_14999.DriveMotion.DriveMethods;
+
 @Autonomous(name="DriveGeneral", group="Autonomous")
 
 public class DriveGeneralTest extends BasicAuto {
@@ -15,11 +17,11 @@ public class DriveGeneralTest extends BasicAuto {
 
         runtime.reset();
 
-        drv.driveGeneral("Forward", 24, prm.DRIVE_POWER_LIMIT, "Back 10 inches",this);
+        drv.driveGeneral(DriveMethods.moveDirection.FwdBack, 24, prm.DRIVE_POWER_LIMIT, "Back 24 inches",this);
         sleep(1000);
-        drv.driveGeneral("Right", -24, prm.DRIVE_POWER_LIMIT, "Left 10 inches",this);
+        drv.driveGeneral(DriveMethods.moveDirection.RightLeft, -24, prm.DRIVE_POWER_LIMIT, "Left 24 inches",this);
         sleep(1000);
-        drv.driveGeneral("Rotate", 90, prm.DRIVE_POWER_LIMIT, "Clockwise 90 degrees",this);
+        drv.driveGeneral(DriveMethods.moveDirection.Rotate, 90, prm.DRIVE_POWER_LIMIT, "Clockwise 90 degrees",this);
         sleep(1000);
     }
 }
