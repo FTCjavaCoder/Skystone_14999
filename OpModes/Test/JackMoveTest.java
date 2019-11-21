e2e1d681847b16748c804434e3c41cd2e8f33a81
Caleb <calebspiess@gmail.com>
2019-11-20 17:17:09 -0700

    Jack move test

--- OpModes/Test/JackMoveTest.java
+++ OpModes/Test/JackMoveTest.java
@@ -2,6 +2,9 @@ package Skystone_14999.OpModes.Test;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+
 import Skystone_14999.DriveMotion.DriveMethods;
 import Skystone_14999.OpModes.Autonomous.BasicAuto;
 
@@ -11,6 +14,18 @@ public class JackMoveTest extends BasicAuto {
 
     @Override
     public void runOpMode() {
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = cons.VUFORIA_KEY;
+        parameters.cameraDirection   = cons.CAMERA_CHOICE;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        targetsSkyStone = this.vuforia.loadTrackablesFromAsset("Skystone");
+        //all above lines need to be all autonomous OpMode's runOpMode before initialization
 
         initialize();
 
